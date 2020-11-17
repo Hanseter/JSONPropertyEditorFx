2241254c6c70935a7b7d8a6bf1ff99f4affe9ac3
1andOlaf <66435397+1andOlaf@users.noreply.github.com>
2020-11-17 08:37:46 +0100

    Fix display of explicit null when there is a default value.

--- src/main/kotlin/com/github/hanseter/json/editor/types/ModelControlSynchronizer.kt
+++ src/main/kotlin/com/github/hanseter/json/editor/types/ModelControlSynchronizer.kt
@@ -3,6 +3,7 @@ package com.github.hanseter.json.editor.types
 import javafx.beans.property.Property
 import javafx.beans.value.ChangeListener
 import javafx.beans.value.ObservableValue
+import org.json.JSONObject
 
 class ModelControlSynchronizer<T>(private val controlProp: Property<T>, private val model: TypeModel<T?, *>) : ChangeListener<T?> {
     init {
@@ -15,7 +16,8 @@ class ModelControlSynchronizer<T>(private val controlProp: Property<T>, private 
 
     fun modelChanged() {
         controlProp.removeListener(this)
-        val newVal = model.value ?: model.defaultValue
+        val newVal = model.value
+                ?: (if (model.rawValue == JSONObject.NULL) null else model.defaultValue)
         if (controlProp.value != newVal) {
             controlProp.value = newVal
         }
