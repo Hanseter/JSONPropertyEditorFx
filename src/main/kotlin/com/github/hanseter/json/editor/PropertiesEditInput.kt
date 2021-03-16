278808c6340c3576e173bf48b8e6528af8a54260
OneAndOlaf <66435397+1andOlaf@users.noreply.github.com>
2021-03-16 08:54:02 +0100

    Add a tiny amount of documentation.

--- src/main/kotlin/com/github/hanseter/json/editor/PropertiesEditInput.kt
+++ src/main/kotlin/com/github/hanseter/json/editor/PropertiesEditInput.kt
@@ -2,9 +2,21 @@ package com.github.hanseter.json.editor
 
 import org.json.JSONObject
 
+/**
+ * The data which various user-defined functions receive as input.
+ *
+ * It allows access to the current document data, as well as the schema.
+ */
+data class PropertiesEditInput(
+        /**
+         * Gets the current document data.
+         */
+        val data: JSONObject,
+        private val _schema: JSONObject) {
 
-data class PropertiesEditInput(val data: JSONObject, private val _schema: JSONObject) {
-
+    /**
+     * Gets a copy of the current schema.
+     */
     val schema
         get() = SchemaNormalizer.deepCopy(_schema) as JSONObject
 
